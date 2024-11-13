c982df2f91177e7200e58da54b2fdd7f2a043e53
david <david.kim.041005@gmail.com>
2024-11-13 01:44:18 -0800

    finished spinhalf

--- qutip-test.py
+++ qutip-test.py
@@ -1,4 +1,23 @@
-import matplotlib.pyplot as plt
 import numpy as np
-import qutip
+from scipy import constants
+import matplotlib.pyplot as plt
+from qutip import * 
+import qutip as qt
 
+def qubit_time_evolution(hamiltonian, alpha, beta, tlist):
+    norm = np.sqrt(abs(alpha)**2 + abs(beta)**2)
+    alpha, beta = alpha / norm, beta / norm
+    
+    initial_state = alpha * basis(2, 0) + beta * basis(2, 1)
+    
+    result = sesolve(hamiltonian, initial_state, tlist)
+
+    bloch = Bloch()
+    bloch.add_states(result.states)
+    
+    bloch.make_sphere()
+    plt.show()
+
+    def output_points(alpha, beta):
+        theta = 2 * np.arccos(alpha)
+        
--- spinhalf.py
+++ spinhalf.py
@@ -1,18 +1,19 @@
 import numpy as np
+from scipy import constants
 import matplotlib.pyplot as plt
 from qutip import * 
 import qutip as qt
 
 # params
 omega = 1.0 # H = omega * sigma_z
-alpha = 1
-beta = 1  # psi = alpha |0> + beta |1>
-t_list = np.linspace(0, 400)  # time points
+alpha = 1 / 2
+beta = np.sqrt(3) / 2 # psi = alpha |0> + beta |1>
+t_list = np.linspace(0, 100, 100)  # time points
 psi0 = alpha * basis(2, 0) + beta * basis(2, 1)  # qobj init state
 
 H = omega * sigmaz()
 
-def qubit_time_evolution(hamiltonian, alpha, beta, tlist):
+def qubit_time_evolution_qutip(hamiltonian, alpha, beta, tlist):
     norm = np.sqrt(abs(alpha)**2 + abs(beta)**2)
     alpha, beta = alpha / norm, beta / norm
     
@@ -26,21 +27,20 @@ def qubit_time_evolution(hamiltonian, alpha, beta, tlist):
     bloch.make_sphere()
     plt.show()
 
-def qubit_time_evolution2 (hamiltonian, psi0, tlist):
-    result = mesolve(hamiltonian, psi0, tlist, [], [])
-    b = Bloch()
+def qubit_time_evolution_matplot(alpha, beta):
+    ax = plt.figure().add_subplot(projection='3d')
+    t = np.linspace(0, 100, 100)
+    alpha_r = alpha * (np.cos(omega*t/constants.hbar) - 1j * np.sin(omega*t/constants.hbar))
+    beta_r = beta * (np.cos(omega*t/constants.hbar) + 1j * np.sin(omega*t/constants.hbar))
+    
+    z = alpha**2 - beta**2
+    y = 2 * np.imag(np.conjugate(alpha_r) * beta_r)
+    x = 2 * np.real(np.conjugate(alpha_r) * beta_r)
 
-    for state in result.states:
-        rho = state.proj()
-        x = 2 * np.real(rho[0, 1])
-        y = 2 * np.imag(rho[1, 0])
-        z = rho[0, 0] - rho[1, 1]
-        
-        b.add_points([x, y, z])
-        b.make_sphere()
+    ax.plot(x, y, z, label='parametric curve')
+    ax.legend()
+
     plt.show()
-
-def qubit_time_evolution3 (hamiltonian, psi0, tlist):
-    print ('a')
-
-qubit_time_evolution(H, alpha, beta, t_list)
+    
+qubit_time_evolution_matplot(alpha, beta)
+qubit_time_evolution_qutip(H, alpha, beta, t_list)
